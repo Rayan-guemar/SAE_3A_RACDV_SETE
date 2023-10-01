8983ff67bffa6cfd6ec9ef95665c5b6b0b1e782e
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2023-10-02 01:47:14 +0200

    route festival/{id}

--- src/Controller/FestivalController.php
+++ src/Controller/FestivalController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\FestivalRepository;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +15,21 @@ class FestivalController extends AbstractController {
     public function index(): Response {
         return $this->render('festival/index.html.twig', [
             'controller_name' => 'FestivalController',
+        ]);
+    }
+
+    #[Route('/festival/{id}', name: 'app_festival')]
+    public function festival(int $id, FestivalRepository $festivalRepository): Response {
+
+        $f = $festivalRepository->findOneBy(['id' => $id]);
+
+        if (!$f) {
+            throw $this->createNotFoundException('Festival non trouvé');
+        }
+
+        return $this->render('festival/festival.html.twig', [
+            'controller_name' => 'FestivalController',
+            'festival' => $f,
         ]);
     }
 }
