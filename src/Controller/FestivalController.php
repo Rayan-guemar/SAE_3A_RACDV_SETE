0ed75ff8dcc63a779c2dae708f85e73bb2f8b793
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2023-10-03 19:33:46 +0200

    route demandes et accept

--- src/Controller/FestivalController.php
+++ src/Controller/FestivalController.php
@@ -7,16 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\DemandeFestival;
+use App\Entity\Festival;
 use App\Form\DemandeFestivalType;
+use App\Repository\DemandeFestivalRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class FestivalController extends AbstractController
-{
+class FestivalController extends AbstractController {
     #[Route('/', name: 'accueil')]
-    public function index(FestivalRepository $repository): Response
-    {
-        $festivals=$repository->findAll();
+    public function index(FestivalRepository $repository): Response {
+        $festivals = $repository->findAll();
         return $this->render('festival/index.html.twig', [
             'controller_name' => 'FestivalController',
             'festivals' => $festivals
@@ -24,15 +24,14 @@ class FestivalController extends AbstractController
     }
 
     #[Route('/festival/ask', name: 'festival_ask')]
-    public function ask(Request $req, EntityManagerInterface $em ): Response
-    {
+    public function ask(Request $req, EntityManagerInterface $em): Response {
         $demandeFestival = new DemandeFestival();
 
         $form = $this->createForm(DemandeFestivalType::class, $demandeFestival);
 
         $form->handleRequest($req);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $demandeFestival->setOrganisateurFestival($this->getUser());
             $em->persist($demandeFestival);
             $em->flush();
@@ -43,5 +42,43 @@ class FestivalController extends AbstractController
             'controller_name' => 'FestivalController',
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/festival/demandes', name: 'festival_demandes')]
+    public function demandes(Request $req, DemandeFestivalRepository $demandeFestivalRepository): Response {
+
+
+        // TODO : vérifier que l'utilisateur est bien un admin
+        $demandesFestivals = $demandeFestivalRepository->findAll();
+
+        return $this->render('festival/demandes.html.twig', [
+            'controller_name' => 'FestivalController',
+            'demandes' => $demandesFestivals
+        ]);
+    }
+
+    #[Route('/festival/accept/{id}', name: 'accept_festival_demande')]
+    public function accept(Request $req, DemandeFestivalRepository $demandeFestivalRepository, EntityManagerInterface $em, int $id): Response {
+        // TODO : vérifier que l'utilisateur est bien un admin
+        $demandeFestival = $demandeFestivalRepository->find($id);
+        if ($demandeFestival === null) {
+            throw $this->createNotFoundException('Demande de festival non trouvée');
+        }
+
+        $festivals = new Festival();
+        $festivals->setNom($demandeFestival->getNomFestival());
+        $festivals->setDateDebut($demandeFestival->getDateDebutFestival());
+        $festivals->setDateFin($demandeFestival->getDateFinFestival());
+        $festivals->setDescription($demandeFestival->getDescriptionFestival());
+        $festivals->setOrganisateur($demandeFestival->getOrganisateurFestival());
+        $festivals->setLieu($demandeFestival->getLieuFestival());
+        $festivals->setAffiche($demandeFestival->getAfficheFestival());
+
+        $em->persist($festivals);
+        $em->remove($demandeFestival);
+        $em->flush();
+
+
+        return $this->redirectToRoute('festival_demandes');
     }
 }
