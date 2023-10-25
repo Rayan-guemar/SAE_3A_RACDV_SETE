2d62c501fa612efea05c52b9648eab2d8281fc5f
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2023-10-25 10:17:02 +0200

    Creneau, Tache, Poste, Disponibilite with doctrine

--- festiflux/src/Entity/Creneaux.php
+++ festiflux/src/Entity/Creneaux.php
@@ -13,8 +13,7 @@ use Doctrine\ORM\Mapping\InverseJoinColumn;
 
 
 #[ORM\Entity(repositoryClass: CreneauxRepository::class)]
-class Creneaux
-{
+class Creneaux {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -33,111 +32,99 @@ class Creneaux
     #[ORM\ManyToOne(inversedBy: 'disponibilites')]
     private ?Utilisateur $utilisateurDisponible = null;
 
-    #[ORM\ManyToMany(targetEntity: Utilisateur::class, mappedBy: 'creneauxAffectes')]
-    private Collection $utilisateursAffectes;
-
     #[ORM\OneToOne(mappedBy: 'crenaux', cascade: ['persist', 'remove'])]
     private ?Tache $tache = null;
 
-    public function __construct()
-    {
-        $this->utilisateursAffectes = new ArrayCollection();
+    #[ORM\OneToMany(mappedBy: 'creneau', targetEntity: Disponibilite::class, orphanRemoval: true)]
+    private Collection $disponibilites;
+
+    public function __construct() {
         $this->utilisateurDisponible;
+        $this->disponibilites = new ArrayCollection();
     }
 
-    public function getId(): ?int
-    {
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getDateDebut(): ?\DateTimeInterface
-    {
+    public function getDateDebut(): ?\DateTimeInterface {
         return $this->dateDebut;
     }
 
-    public function setDateDebut(\DateTimeInterface $dateDebut): static
-    {
+    public function setDateDebut(\DateTimeInterface $dateDebut): static {
         $this->dateDebut = $dateDebut;
 
         return $this;
     }
 
-    public function getDateFin(): ?\DateTimeInterface
-    {
+    public function getDateFin(): ?\DateTimeInterface {
         return $this->dateFin;
     }
 
-    public function setDateFin(\DateTimeInterface $dateFin): static
-    {
+    public function setDateFin(\DateTimeInterface $dateFin): static {
         $this->dateFin = $dateFin;
 
         return $this;
     }
 
-    public function getFestival(): ?Festival
-    {
+    public function getFestival(): ?Festival {
         return $this->festival;
     }
 
-    public function setFestival(?Festival $festival): static
-    {
+    public function setFestival(?Festival $festival): static {
         $this->festival = $festival;
 
         return $this;
     }
 
-    public function getUtilisateurDisponible(): ?Utilisateur
-    {
+    public function getUtilisateurDisponible(): ?Utilisateur {
         return $this->utilisateurDisponible;
     }
 
-    public function setUtilisateurDisponible(?Utilisateur $utilisateurDisponible): static
-    {
+    public function setUtilisateurDisponible(?Utilisateur $utilisateurDisponible): static {
         $this->utilisateurDisponible = $utilisateurDisponible;
 
         return $this;
     }
 
-    /**
-     * @return Collection<int, Utilisateur>
-     */
-    public function getUtilisateursAffectes(): Collection
-    {
-        return $this->utilisateursAffectes;
-    }
-
-    public function addUtilisateursAffecte(Utilisateur $utilisateursAffecte): static
-    {
-        if (!$this->utilisateursAffectes->contains($utilisateursAffecte)) {
-            $this->utilisateursAffectes->add($utilisateursAffecte);
-            $utilisateursAffecte->addCreneauxAffecte($this);
-        }
-
-        return $this;
-    }
-
-    public function removeUtilisateursAffecte(Utilisateur $utilisateursAffecte): static
-    {
-        if ($this->utilisateursAffectes->removeElement($utilisateursAffecte)) {
-            $utilisateursAffecte->removeCreneauxAffecte($this);
-        }
-
-        return $this;
-    }
-
-    public function getTache(): ?Tache
-    {
+    public function getTache(): ?Tache {
         return $this->tache;
     }
 
-    public function setTache(Tache $tache): static
-    {
+    public function setTache(Tache $tache): static {
         // set the owning side of the relation if necessary
         if ($tache->getCrenaux() !== $this) {
             $tache->setCrenaux($this);
         }
 
         $this->tache = $tache;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Disponibilite>
+     */
+    public function getDisponibilites(): Collection {
+        return $this->disponibilites;
+    }
+
+    public function addDisponibilite(Disponibilite $disponibilite): static {
+        if (!$this->disponibilites->contains($disponibilite)) {
+            $this->disponibilites->add($disponibilite);
+            $disponibilite->setCreneau($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDisponibilite(Disponibilite $disponibilite): static {
+        if ($this->disponibilites->removeElement($disponibilite)) {
+            // set the owning side to null (unless already changed)
+            if ($disponibilite->getCreneau() === $this) {
+                $disponibilite->setCreneau(null);
+            }
+        }
 
         return $this;
     }
