a85f7851a60a2634f8423df68f888663f5ce4754
bettingers <95344896+bettingers@users.noreply.github.com>
2023-10-29 10:54:00 +0100

    GEN-73 Modifier profil (#29)
    * Modifier un profil
    
    * Modifier profil
    
    ---------
    
    Co-authored-by: Tanguy Macé <95345090+Yuyugnat@users.noreply.github.com>

--- festiflux/src/Controller/UtilisateurController.php
+++ festiflux/src/Controller/UtilisateurController.php
@@ -2,20 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Festival;
 use App\Entity\Utilisateur;
 use App\Form\InscriptionType;
+use App\Form\ModifierFestivalType;
+use App\Form\ModifierProfilType;
 use App\Repository\FestivalRepository;
 use App\Repository\TacheRepository;
 use App\Repository\UtilisateurRepository;
 use App\Service\Ical\IcalBuilder;
 use App\Service\Ical\Event;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use App\Service\FlashMessageService;
 use App\Service\FlashMessageType;
-
 
 class UtilisateurController extends AbstractController {
 
@@ -71,7 +77,33 @@ class UtilisateurController extends AbstractController {
         $ical->build();
         return $this->redirectToRoute('app_festival_all');
     }
+  
+    #[Route('/user/profile/{id}/edit', name: 'app_profile_edit')]
+    public function edit(UtilisateurRepository $repository, #[MapEntity] Utilisateur $utilisateur, Request $request, EntityManagerInterface $em, ): Response {
 
+        if (!$utilisateur) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+
+        $form = $this->createForm(ModifierProfilType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $em->flush();
+            $this->addFlash('success', 'Votre profil a été modifié avec succès.');
+            return $this->redirectToRoute('app_user_profile', ['id' => $utilisateur->getId()]);
+        }
+
+        return $this->render('utilisateur/modifierProfil.html.twig', [
+            'controller_name' => 'UtilisateurController',
+            'form' => $form->createView(),
+            'utilisateur' => $utilisateur,
+        ]);
+    }
+                         
     #[Route('/user/{id}/task/{idTask}/add', name: 'app_user_task_add', methods: ['GET'])]
     public function user_task_add(int $id, int $idTask, UtilisateurRepository $user, TacheRepository $tache, EntityManagerInterface $em, FlashMessageService $fm): Response {
 
@@ -99,5 +131,4 @@ class UtilisateurController extends AbstractController {
 
         return $this->redirectToRoute('home');
     }
-
 }
