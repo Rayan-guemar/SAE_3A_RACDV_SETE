5ea5d04ecefc0fcd81a850c333434c157f73f6f4
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2023-11-01 17:46:00 +0100

    isResponsableOrOrganisateur attribut

--- festiflux/public/js/planning/Planning.js
+++ festiflux/public/js/planning/Planning.js
@@ -8,21 +8,22 @@ import { Tache } from "./Tache.js";
  * Classe représentant un planning.
  */
 export class Planning {
-  /**
-   * Crée une instance de la classe Planning.
-   * @constructor
-   * @param {number} festId - L'ID du festival.
-   * @param {Date} dateDebut - La date de début du planning.
-   * @param {Date} dateFin - La date de fin du planning.
-   * @param {Poste[]} postes - Les postes associés au planning.
-   * @param {Tache[]} taches - Les créneaux du planning.
-   */
-  constructor(festId, dateDebut, dateFin, isResponsable, isOrganisateur) {
-    this.festId = festId;
-    this.html = document.getElementById("planning");
-    this.dateDebut = new Date(dateDebut);
-    this.dateFin = new Date(dateFin);
-    this.dateFin.setHours(23, 59, 59, 999);
+	/**
+	 * Crée une instance de la classe Planning.
+	 * @constructor
+	 * @param {number} festId - L'ID du festival.
+	 * @param {Date} dateDebut - La date de début du planning.
+	 * @param {Date} dateFin - La date de fin du planning.
+	 * @param {Poste[]} postes - Les postes associés au planning.
+	 * @param {Tache[]} taches - Les créneaux du planning.
+	 */
+	constructor(festId, dateDebut, dateFin, isResponsableOrOrganisateur) {
+		this.festId = festId;
+		this.html = document.getElementById('planning');
+		this.dateDebut = new Date(dateDebut);
+		this.dateFin = new Date(dateFin);
+		this.dateFin.setHours(23, 59, 59, 999);
+		this.isResponsableOrOrganisateur = isResponsableOrOrganisateur;
 
     /**
      * Les postes associés au planning.
@@ -36,22 +37,21 @@ export class Planning {
      */
     this.taches = [];
 
-    /**
-     * Les Bénévoles associés au planning.
-     * @type {Benevole[]}
-     * */
-    this.benevoles = [];
+		if (this.isResponsableOrOrganisateur) {
+			this.addCreneauxBtn = document.getElementById('add-creneau-btn');
+			this.addCreneauxForm = document.getElementById('add-creneau');
+			this.creneauDescription = document.getElementById('creneau-description');
+			this.creneauNombreBenevole = document.getElementById('creneau-nombre-benevole');
+			this.startCreneauxInput = document.getElementById('start-creneau');
+			this.endCreneauxInput = document.getElementById('end-creneau');
+			this.creneauPosteSelect = document.getElementById('creneau-poste-select');
+			this.createCreneauxBtn = document.getElementById('create-creneau-btn');
 
-    this.addCreneauxBtn = document.getElementById("add-creneau-btn");
-    this.addCreneauxForm = document.getElementById("add-creneau");
-    this.creneauDescription = document.getElementById("creneau-description");
-    this.creneauNombreBenevole = document.getElementById(
-      "creneau-nombre-benevole"
-    );
-    this.startCreneauxInput = document.getElementById("start-creneau");
-    this.endCreneauxInput = document.getElementById("end-creneau");
-    this.creneauPosteSelect = document.getElementById("creneau-poste-select");
-    this.createCreneauxBtn = document.getElementById("create-creneau-btn");
+			this.addPostebtn = document.getElementById('add-poste-btn');
+			this.addPosteForm = document.getElementById('add-poste');
+			this.createPosteBtn = document.getElementById('create-poste-btn');
+			this.createPosteInput = document.getElementById('poste-name');
+		}
 
     this.addPostebtn = document.getElementById("add-poste-btn");
     this.addPosteForm = document.getElementById("add-poste");
@@ -373,25 +373,35 @@ export class Planning {
     return dateToDayMap;
   };
 
-  renderTaches = () => {
-    const dateToDayMap = this.getDateToDayMapping();
+	/**
+	 *
+	 * @param {Tache} tache
+	 * @param {HTMLDivElement} dayDiv
+	 */
+	renderOneTache = (t, dayDiv) => {
+		const taskDiv = document.createElement('div');
+		taskDiv.classList.add('task');
+		taskDiv.innerHTML = `
+            <div class="name">${encodedStr(t.poste.nom)}</div>
+            <div class="creneau">${encodedStr(`${getDateHours2Digits(t.creneau.debut)} - ${getDateHours2Digits(t.creneau.fin)}`)}</div>
+        `;
 
-    for (const d of dateToDayMap.values()) {
-      [...d.getElementsByClassName("task")].forEach((t) => t.remove());
-    }
+		taskDiv.style.top = `${(t.creneau.debut.getHours() / 24) * 100}%`;
+		taskDiv.style.height = `${((t.creneau.fin.getHours() - t.creneau.debut.getHours()) / 24) * 100}%`;
+		dayDiv.appendChild(taskDiv);
+	};
 
-    for (const t of this.taches) {
-      const date = new Date(t.creneau.debut);
-      const dayDiv = dateToDayMap.get(date.toDateString());
-      if (!dayDiv) {
-        console.error(`Aucun div de jour trouvé pour la date ${date}`);
-        continue;
-      }
-
-      const taskDiv = document.createElement("div");
-      taskDiv.setAttribute("tache-id", t.id);
-      taskDiv.classList.add("task");
-      taskDiv.innerHTML = `
+	/**
+	 *
+	 * @param {Tache[]} taches
+	 * @param {HTMLDivElement} dayDiv
+	 */
+	renderMultipleTaches = (taches, dayDiv) => {
+		for (let i = 0; i < taches.length; i++) {
+			const t = taches[i];
+			const taskDiv = document.createElement('div');
+			taskDiv.classList.add('task');
+			taskDiv.innerHTML = `
             <div class="name">${encodedStr(t.poste.nom)}</div>
             <div class="creneau">${encodedStr(
               `${getDateHours2Digits(t.creneau.debut)} - ${getDateHours2Digits(
--- festiflux/public/js/planning/Tache.js
+++ festiflux/public/js/planning/Tache.js
@@ -1,3 +1,6 @@
+import { Creneau } from './Creneau.js';
+import { Poste } from './Poste.js';
+
 /**
  * Classe représentant une tâche à effectuer lors d'un événement.
  * @class
@@ -18,5 +21,17 @@ export class Tache {
 		this.nbBenevole = nbBenevole;
 		this.poste = poste;
 		this.creneau = creneau;
+	}
+
+	/**
+	 * Vérifie si les créneau horaires de deux tâches se chevauchent.
+	 * @param {Tache} tache
+	 * @returns {boolean} Vrai si les créneaux horaires se chevauchent, faux sinon.
+	 */
+	overrides(tache) {
+		const res = (this.creneau.debut < tache.creneau.fin && this.creneau.debut > this.creneau.debut) || (this.creneau.fin > tache.creneau.debut && this.creneau.fin < tache.creneau.fin);
+		if (res) console.log('Comparing', this.creneau, 'with', tache.creneau);
+		return res;
+		// return (this.creneau.debut < tache.creneau.fin && this.creneau.debut > this.creneau.debut) || (this.creneau.fin > tache.creneau.debut && this.creneau.fin < tache.creneau.fin);
 	}
 }
