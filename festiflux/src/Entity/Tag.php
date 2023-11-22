fbcb70177d191b90dcc364b1729b69e51fe0786c
adam henchiri <adam.henchiri@etu.umontpellier.fr>
2023-11-22 13:58:39 +0100

    tag entity+repo

---
+++ festiflux/src/Entity/Tag.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TagRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\ManyToMany(targetEntity: Festival::class, mappedBy: 'tags')]
+    private Collection $festivals;
+
+    public function __construct()
+    {
+        $this->festivals = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Festival>
+     */
+    public function getFestivals(): Collection
+    {
+        return $this->festivals;
+    }
+
+    public function addFestival(Festival $festival): static
+    {
+        if (!$this->festivals->contains($festival)) {
+            $this->festivals->add($festival);
+            $festival->addTag($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFestival(Festival $festival): static
+    {
+        if ($this->festivals->removeElement($festival)) {
+            $festival->removeTag($this);
+        }
+
+        return $this;
+    }
+}
