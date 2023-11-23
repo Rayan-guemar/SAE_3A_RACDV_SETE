0bc67b59862bac8281547a426a3654d6d93d25ad
adam henchiri <adam.henchiri@etu.umontpellier.fr>
2023-11-23 21:24:50 +0100

    3 préférences done il manque le css !

--- festiflux/public/js/addLikePost.js
+++ festiflux/public/js/addLikePost.js
@@ -1,15 +1,24 @@
 const checkboxs = document.getElementsByClassName('switch');
+var radioButtons = document.querySelectorAll('input[type="radio"]');
 
-[...checkboxs].forEach(checkbox => {
-    const idPoste = checkbox.id;
-    checkbox.addEventListener('change', async function () {
-        const isChecked = checkbox.checked;
-        const URL = Routing.generate(isChecked ? 'app_user_likedPoste_add' : 'app_user_likedPoste_remove', { idPoste: idPoste });
+[...radioButtons].forEach(radioButton => {
+
+    radioButton.addEventListener('change', async function () {
+        var idPoste = radioButton.getAttribute('name').replace('choice_radio', '');
+        var value = radioButton.value;
+
+        var URL = ""
+        if (value==="0"){
+            URL = Routing.generate('app_user_likedPoste_add',{ idPoste: idPoste })
+        }else if (value==="-1"){
+            URL = Routing.generate('app_user_likedPoste_remove',{ idPoste: idPoste })
+        }else{
+            URL = Routing.generate('app_user_AdorePoste_add',{ idPoste: idPoste })
+        }
         try {
             await fetch(URL);
         } catch (error) {
             console.log(error);
-            checkbox.checked = !isChecked;
         }
     });
 });
