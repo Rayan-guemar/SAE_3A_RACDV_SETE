cb197eabb54e0067fa56c8bc641503479393ea8c
Nathan-Brtn <103536606+Nathan-Brtn@users.noreply.github.com>
2023-11-24 13:54:02 +0100

    Modif bdd user (#69)
    * done pour la creation de festival
    pending pour les creneaux
    
    * added forgot password (#41)
    
    * GEN-30 affecter benevole creneaux (#45)
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * rebase
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * rebase
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * fixed bug
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * rebase
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * added new table and fixed database
    
    * added route to add benevole to tache
    
    * fixed bug
    
    * ajout du front pour affecter un benevole a un creneau
    
    * GEN-69 afficher creneau sur plusieurs jours (#49)
    
    * isResponsableOrOrganisateur attribut
    
    * a
    
    * poste color with hash + spacing tasks
    
    * remove useless
    
    * isResponsableOrOrganisateur attribut
    
    * a
    
    * poste color with hash + spacing tasks
    
    * remove useless
    
    * add renderTaches again
    
    * final fix insh
    
    ---------
    
    Co-authored-by: Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
    
    * GEN-31 ajout d'un controller pour gérer l'inscription (#47)
    
    * ajout d'un controller pour gérer l'inscription
    
    * ajout d'un controller pour gérer l'inscription
    
    ---------
    
    Co-authored-by: bettingers <95344896+bettingers@users.noreply.github.com>
    
    * added address on Lieu (#51)
    
    * modif entity utilisateur
    
    * Modification entité user
    
    * Modification entité user
    
    * Modification entité user
    
    ---------
    
    Co-authored-by: adam henchiri <adam.henchiri@etu.umontpellier.fr>
    Co-authored-by: LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
    Co-authored-by: Tanguy Macé <95345090+Yuyugnat@users.noreply.github.com>
    Co-authored-by: Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
    Co-authored-by: bettingers <95344896+bettingers@users.noreply.github.com>

---
+++ festiflux/migrations/Version20231102213548.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231102213548 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tache ADD CONSTRAINT FK_93872075A0905086 FOREIGN KEY (poste_id) REFERENCES poste (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tache DROP FOREIGN KEY FK_93872075A0905086');
+    }
+}
