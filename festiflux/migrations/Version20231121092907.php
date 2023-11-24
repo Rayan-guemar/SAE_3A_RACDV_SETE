b6a1f4829fd689831d69f81352c2534d920d81d5
Nathan <nexon70000@gmail.com>
2023-11-24 13:27:47 +0100

    Modification entité user

---
+++ festiflux/migrations/Version20231121092907.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231121092907 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE poste ADD description VARCHAR(255) NOT NULL, DROP couleur');
+        $this->addSql('ALTER TABLE tache CHANGE description remarque VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE poste ADD couleur VARCHAR(7) DEFAULT NULL, DROP description');
+        $this->addSql('ALTER TABLE tache CHANGE remarque description VARCHAR(255) NOT NULL');
+    }
+}
