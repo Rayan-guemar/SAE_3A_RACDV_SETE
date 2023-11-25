d9b0f1aed5a9238fad68e52dd3bac4df4d529d5a
Tanguy Macé <95345090+Yuyugnat@users.noreply.github.com>
2023-11-25 12:41:09 +0100

    beginning vuejs version

--- festiflux/assets/svelte/stores/posteOpened.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const currentPoste = writable({
-    id: null,
-    name: '',
-    color: '#347deb',
-    desc: '',
-});
-export const posteOpened = writable(false);
