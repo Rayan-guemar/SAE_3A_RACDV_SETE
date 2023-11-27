c945a2d3920fef72f5d3ca042f3049d4d8864cbd
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2023-11-27 16:50:19 +0100

    added lieu

--- festiflux/assets/vue/controllers/InfoTache.vue
+++ festiflux/assets/vue/controllers/InfoTache.vue
@@ -12,7 +12,7 @@ defineProps<Props>();
 
 <template>
     <div class="tache-info_wrapper" >
-        <div class="tache-info_nom" >{{ tache.poste.nom }}</div>
+        <div class="tache-info_nom"  >{{ tache.poste.nom }}</div>
         <div class="tache-info_desc" >{{ tache.description }}</div>
         <div class="tache-info_lieu " >{{ tache.lieu }}</div>
         <div class="tache-info_benevoles" >{{ tache.nbBenevole }} bénévoles requis</div>
