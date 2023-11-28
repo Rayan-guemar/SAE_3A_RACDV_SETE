72569bd325cbeeb4b2c42f679c283ba194a9cb6d
geoffrey <geoffrey.pierre@etu.umontpellier.fr>
2023-11-28 23:06:11 +0100

    all fest map
    all fest map

--- festiflux/public/js/AllMapDetailFest.js
+++ festiflux/public/js/AllMapDetailFest.js
@@ -1,13 +1,14 @@
-
 async function getAllFestival() {
+
     // Initialisation de la carte
-    var mymap = L.map('mapid').setView([46.485935, 2.553603], 6);
+    var map = L.map('map').setView([46.485935, 2.553603], 6);
 
     // Ajout de la carte OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
+    }).addTo(map);
+
     const response = await fetch(Routing.generate('app_all_festival'));
     if (!response.ok) {
         throw new Error(await response.text());
@@ -19,7 +20,7 @@ async function getAllFestival() {
             data.forEach(festival => {
                 let lat = festival.latitude;
                 let lon = festival.longitude;
-                marker = L.marker([lat, lon]).addTo(mymap);
+                marker = L.marker([lat, lon]).addTo(map);
                 //Sets the view of the map (geographical center and zoom) with the given animation options.
             });
         } catch (error) {
