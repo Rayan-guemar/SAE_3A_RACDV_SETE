4b2d12fa96b82ec08cce558ca70a8cff7402f71a
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2023-12-01 05:22:58 +0000

    Fix forgot pwd (#105)
    * draft: ajout indispo
    
    * fix lucas' bugs
    
    * pastille nbDIspo fini ?
    
    * fix pastille nbDispo
    
    * pastille like
    
    * add impo
    
    * reset mdp francais + css form
    
    * fix css
    
    ---------
    
    Co-authored-by: Tanguy Macé <95345090+Yuyugnat@users.noreply.github.com>

--- festiflux/src/Form/ChangePasswordFormType.php
+++ festiflux/src/Form/ChangePasswordFormType.php
@@ -34,10 +34,10 @@ class ChangePasswordFormType extends AbstractType
                             'max' => 4096,
                         ]),
                     ],
-                    'label' => 'New password',
+                    'label' => 'Nouveau mot de passe',
                 ],
                 'second_options' => [
-                    'label' => 'Repeat Password',
+                    'label' => 'Répéter le mot de passe',
                 ],
                 'invalid_message' => 'The password fields must match.',
                 // Instead of being set onto the object directly,
