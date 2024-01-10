f6d1099f6883b4f960e6b51646d04a3dd2f4fe6e
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2024-01-10 02:35:07 +0100

    validation controller

--- festiflux/src/Entity/Validation.php
+++ festiflux/src/Entity/Validation.php
@@ -3,11 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\ValidationRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ValidationRepository::class)]
-class Validation
-{
+class Validation {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -17,20 +17,38 @@ class Validation
     #[ORM\JoinColumn(nullable: false)]
     private ?Festival $festival = null;
 
-    public function getId(): ?int
-    {
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $status = 0;
+
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getFestival(): ?Festival
-    {
+    public function getFestival(): ?Festival {
         return $this->festival;
     }
 
-    public function setFestival(?Festival $festival): static
-    {
+    public function setFestival(?Festival $festival): static {
         $this->festival = $festival;
 
         return $this;
+    }
+
+    public function getStatus(): ?int {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function accept() {
+        $this->setStatus(1);
+    }
+
+    public function reject() {
+        $this->setStatus(-1);
     }
 }
