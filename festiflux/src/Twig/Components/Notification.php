ed018a06f1b95111927c7e8cdee01b92f3cc2e50
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-02-19 15:23:03 +0100

    Refactor Notification class and fix image paths in user_festivals.html.twig

--- festiflux/src/Twig/Components/Notification.php
+++ festiflux/src/Twig/Components/Notification.php
@@ -2,31 +2,33 @@
 
 namespace App\Twig\Components;
 
-use App\Repository\DemandeFestivalRepository;
+use App\Repository\ValidationRepository;
 use App\Repository\FestivalRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 class Notification
-{
-    private DemandeFestivalRepository $demandeRepo;
+{   
+    private ValidationRepository $validRepo;
     private FestivalRepository $festRepo;
     public ?int $userId;
     public string $type;
     public ?int $festId;
 
-    public function __construct(DemandeFestivalRepository $demandeRepo, FestivalRepository $festRepo)
+    public function __construct(ValidationRepository $validRepo, FestivalRepository $festRepo)
     {
-        $this->demandeRepo = $demandeRepo;
+        $this->validRepo = $validRepo;
         $this->festRepo = $festRepo;
     }
 
     function renderNombre(): int
     {
         if($this->type == "demandeFestival"){
+          
+            $validations = $this->validRepo->findBy(['status' => 0]);
 
-            return count($this->demandeRepo->findAll());
-            
+            return count($validations);   
+          
         } else if ($this->type == "allDemandesBenevolat" && $this->userId != null){ 
             
             $res = 0;
