342002224f9251664dff6ad99c3947704225a6cf
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2024-02-21 19:48:57 +0100

    GEN-183 moyen de contact (#143)
    * draft: Update profile page and add contact section
    
    * Add contact functionality and display contacts in user profile
    
    * Update contact section and styles
    
    ---------
    
    Co-authored-by: LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>

---
+++ festiflux/src/Entity/TypeContact.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypeContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypeContactRepository::class)]
+class TypeContact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $linkTemplate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLinkTemplate(): ?string
+    {
+        return $this->linkTemplate;
+    }
+
+    public function setLinkTemplate(string $linkTemplate): static
+    {
+        $this->linkTemplate = $linkTemplate;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+}
