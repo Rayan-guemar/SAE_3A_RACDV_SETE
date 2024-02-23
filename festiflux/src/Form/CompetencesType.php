8f9c45602b9cca1a3f49df9cbba7d68dd8c3534e
geoffrey <geoffrey.pierre@etu.umontpellier.fr>
2024-02-24 00:59:11 +0100

    competences form

---
+++ festiflux/src/Form/CompetencesType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Competences;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetencesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('niveau')
+            ->add('autheur', EntityType::class, [
+                'class' => Utilisateur::class,
+                'choice_label' => 'id',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Competences::class,
+        ]);
+    }
+}
