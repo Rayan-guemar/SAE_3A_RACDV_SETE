4457b3b98522c652787b799f7b051d694a09742a
LuK-CZT <92637584+LuK-CZT@users.noreply.github.com>
2024-03-01 04:15:09 +0100

    Refactor demandeBenevole.js and archive.html.twig

--- festiflux/public/js/demandeBenevole.js
+++ festiflux/public/js/demandeBenevole.js
@@ -1,12 +1,11 @@
-
 let button = document.getElementsByClassName("button-benevole");
-    button[0].addEventListener("click",function (e) {
-        let xhr = new XMLHttpRequest();
+button[0].addEventListener("click", function (e) {
+  let xhr = new XMLHttpRequest();
 
-        let festId = (document.getElementsByClassName("btn btn-primary"))[0].dataset.festivalid;
-        let URL = Routing.generate('app_demandebenevole_add', {'id': festId});
-        xhr.open("GET",URL);
-        //On veut une réponse au format JSON
-        xhr.responseType = "json";
-        xhr.send();
-    });
+  let festId = document.getElementsByClassName("btn")[0].dataset.festivalid;
+  let URL = Routing.generate("app_demandebenevole_add", { id: festId });
+  xhr.open("GET", URL);
+  //On veut une réponse au format JSON
+  xhr.responseType = "json";
+  xhr.send();
+});
